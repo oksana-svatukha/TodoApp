f0c4ad46ebfae698c058b7136952213145d391b3
oksana-svatukha <oksana.svatukha@gmail.com>
2024-11-19 17:16:39 +0200

    Authenticated user

--- routers/auth.py
+++ routers/auth.py
@@ -3,10 +3,10 @@ from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from starlette import status
-
 from database import SessionLocal
 from models import Users
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm
 
 
 router = APIRouter()
@@ -30,7 +30,13 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
-
+def authenticate_user(username: str, password: str, db):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_contex.verify(password, user.hashed_password):
+        return False
+    return True
 
 @router.post("/auth/", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency,
@@ -47,4 +53,11 @@ async def create_user(db: db_dependency,
     db.add(create_user_model)
     db.commit()
 
-    return {"user": "authenticated"}
+
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                                 db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return 'Failed Authentication'
+    return 'Successful Authentication'
