be4ffefb8a5af04d25b90241563e27e12b123786
oksana-svatukha <oksana.svatukha@gmail.com>
2024-11-23 18:16:33 +0200

    Get All Todos (User ID)

--- routers/todos.py
+++ routers/todos.py
@@ -30,8 +30,9 @@ class TodoRequest(BaseModel):
 
 
 @router.get("/", status_code=status.HTTP_200_OK)
-async def read_all(db: db_dependency):
-    return db.query(Todos).all()
+async def read_all(user: user_dependency,
+                   db: db_dependency):
+    return db.query(Todos).filter(Todos.owner_id == user.get('id')).all()
 
 
 
